b31b51eb7714265f2c1a050679526c3fa3f70f93
Hendrik Pienaar <pienaahj@Hendrik-Pienaars-Mac-Pro.local>
2020-11-25 17:30:11 +0200

    json encoding

--- main.go
+++ main.go
@@ -1,5 +1,29 @@
 package main
 
-func main() {
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+)
 
+type person struct {
+	First string
+}
+
+func main() {
+	p1 := person{
+		First: "Jenny",
+	}
+
+	p2 := person{
+		First: "James",
+	}
+
+	xp := []person{p1, p2}
+
+	bs, err := json.Marshal(xp)
+	if err != nil {
+		log.Panic(err)
+	}
+	fmt.Println(string(bs))
 }
