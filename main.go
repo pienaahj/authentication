f025556bd8dfe9c0dd62b63cf5d8f925a39517e1
Hendrik Pienaar <pienaahj@Hendrik-Pienaars-Mac-Pro.local>
2020-11-26 09:22:59 +0200

    ninja exc1

--- main.go
+++ main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"encoding/json"
-	"fmt"
 	"log"
 	"net/http"
 )
@@ -12,60 +11,41 @@ type person struct {
 }
 
 func main() {
-	p1 := person{
-		First: "Jenny",
-	}
 
-	p2 := person{
-		First: "James",
-	}
+	// handle routes
+	http.HandleFunc("/encode", enJSON)
 
-	xp := []person{p1, p2}
-
-	bs, err := json.Marshal(xp)
-	if err != nil {
-		log.Panic(err)
-	}
-	fmt.Println("PRINT JSON ", string(bs))
-
-	xp2 := []person{}
-
-	err = json.Unmarshal(bs, &xp2)
-	if err != nil {
-		log.Panic(err)
-	}
-
-	fmt.Println("back into a Go data structure", xp2)
-
-	// ########################################################################
-
-	// Handle the routes
-	http.HandleFunc("/encode", jsonEncoder)
-	http.HandleFunc("/decode", jsonDecoder)
-
-	// Create a server
+	http.HandleFunc("/decode", deJSON)
+	// Create server
 	http.ListenAndServe(":8080", nil)
 
 }
 
-// Create a function that will handle the routes
-func jsonEncoder(w http.ResponseWriter, r *http.Request) {
-	p1 := person{
-		First: "Jenny",
-	}
-
-	err := json.NewEncoder(w).Encode(p1)
+func deJSON(w http.ResponseWriter, req *http.Request) {
+	xp2 := []person{}
+	err := json.NewDecoder(req.Body).Decode(&xp2)
 	if err != nil {
-		log.Println("Cannot encode bad data: ", err)
+		log.Println("Could not decode data: ", err)
 	}
+	log.Println("Decoded json: ", xp2)
 }
 
-func jsonDecoder(w http.ResponseWriter, r *http.Request) {
-	var p1 person
-	err := json.NewDecoder(r.Body).Decode(&p1)
+func enJSON(w http.ResponseWriter, req *http.Request) {
+	xp := []person{
+		{
+			First: "Jenny",
+		},
+		{
+			First: "James",
+		},
+		{
+			First: "Peter",
+		},
+	}
+	err := json.NewEncoder(w).Encode(xp)
 	if err != nil {
-		log.Println("Could not Decode bad Data: ", err)
+		log.Println("Could not encode data: ", err)
 	}
 
-	log.Println("Person: ", p1)
+	log.Println("Json encoding sent to client")
 }
