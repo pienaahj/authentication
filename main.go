38bcca8b81b2cea008a3fd3469c59f0e222a86fe
Hendrik Pienaar <pienaahj@Hendrik-Pienaars-Mac-Pro.local>
2020-11-26 12:32:05 +0200

    Base64 user:pass example

--- main.go
+++ main.go
@@ -1,51 +1,11 @@
 package main
 
 import (
-	"encoding/json"
-	"log"
-	"net/http"
+	"encoding/base64"
+	"fmt"
 )
 
-type person struct {
-	First string
-}
-
 func main() {
+	fmt.Println(base64.StdEncoding.EncodeToString([]byte("user:pass")))
 
-	// handle routes
-	http.HandleFunc("/encode", enJSON)
-
-	http.HandleFunc("/decode", decJSON)
-	// Create server
-	http.ListenAndServe(":8080", nil)
-
-}
-
-func decJSON(w http.ResponseWriter, req *http.Request) {
-	xp2 := []person{}
-	err := json.NewDecoder(req.Body).Decode(&xp2)
-	if err != nil {
-		log.Println("Could not decode data: ", err)
-	}
-	log.Println("Decoded json: ", xp2)
-}
-
-func enJSON(w http.ResponseWriter, req *http.Request) {
-	xp := []person{
-		{
-			First: "Jenny",
-		},
-		{
-			First: "James",
-		},
-		{
-			First: "Peter",
-		},
-	}
-	err := json.NewEncoder(w).Encode(xp)
-	if err != nil {
-		log.Println("Could not encode data: ", err)
-	}
-
-	log.Println("Json encoding sent to client")
 }
