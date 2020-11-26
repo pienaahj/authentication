cc6d1e06ed7358dc7c53f7961f25f87943a03129
Hendrik Pienaar <pienaahj@Hendrik-Pienaars-Mac-Pro.local>
2020-11-26 16:09:33 +0200

    HMAC functions

--- main.go
+++ main.go
@@ -1,13 +1,21 @@
 package main
 
 import (
+	"crypto/hmac"
+	"crypto/sha512"
 	"fmt"
 	"log"
 
 	"golang.org/x/crypto/bcrypt"
 )
 
+var key = []byte{}
+
 func main() {
+	// Generate the key(needs to be 64 bytes for sha512) insecure generate this by another means example only
+	for i := 1; i <= 64; i++ {
+		key = append(key, byte(i))
+	}
 	// The use of UTF8 enables the password string to be any char even an emogi
 	pass := "123456789"
 	hashPass, err := hashPassword(pass)
@@ -37,4 +45,27 @@ func comparePassword(password string, hashedPass []byte) error {
 		return fmt.Errorf("Invalid password: %w", err)
 	}
 	return nil
+}
+
+//HMAC - Hash Message Authentication Code - signing algorithm.
+func signMessage(msg []byte) ([]byte, error) {
+	h := hmac.New(sha512.New, key)
+	_, err := h.Write(msg)
+	if err != nil {
+		return nil, fmt.Errorf("Error in signMessage while signing message: %w", err)
+	}
+	// Sum appends the current hash and returns it.
+	signature := h.Sum(nil)
+	return signature, nil
+}
+
+// Check the signature and message against the signature
+func checkSig(msg, sig []byte) (bool, error) {
+	// Sign the new message
+	newSig, err := signMessage(msg)
+	if err != nil {
+		return false, fmt.Errorf("Error in checkSig while checking signature of message :%w", err)
+	}
+	same := hmac.Equal(newSig, sig)
+	return same, nil
 }
