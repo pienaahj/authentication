7399ce6d259cae079d8415aea89da9c5994b022a
Hendrik Pienaar <pienaahj@Hendrik-Pienaars-Mac-Pro.local>
2020-12-03 18:25:40 +0200

    Adds JWT logig

--- main.go
+++ main.go
@@ -6,7 +6,9 @@ import (
 	"fmt"
 	"io"
 	"net/http"
-	"strings"
+	"time"
+
+	"github.com/dgrijalva/jwt-go"
 )
 
 func main() {
@@ -14,6 +16,8 @@ func main() {
 	http.HandleFunc("/submit", submit)
 	http.ListenAndServe(":8080", nil)
 }
+
+//getCode uses input data - string and returns an HAMC - string
 func getCode(data string) string {
 	//create the hmac hash
 	h := hmac.New(sha256.New, []byte("ourkey 1234"))
@@ -21,6 +25,32 @@ func getCode(data string) string {
 	io.WriteString(h, data)
 	//convert the hash to a string and return it
 	return fmt.Sprintf("%x", h.Sum(nil))
+}
+
+//getJWT uses input data - string and returns an JWT - string
+func getJWT(data string) (string, error) {
+	//create the key
+	mySigningKey := "ourkey 1234"
+	//create myCustomClaims type
+	type MyCustomClaims struct {
+		Data string `json:"myclaimsfield"`
+		jwt.StandardClaims
+	}
+	claims := MyCustomClaims{
+		data,
+		jwt.StandardClaims{
+			ExpiresAt: time.Now().Add(5 * time.Minute).Unix(), //Unix() type int64
+			Issuer:    "me",
+		},
+	}
+	//create the token
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims) //hmac sha256 signing method
+	//sign the token with your key
+	ss, err := token.SignedString([]byte(mySigningKey)) //hmac requires a []byte type as key
+	if err != nil {
+		return "", fmt.Errorf("Could not sign token in SignedString: %w", err)
+	}
+	return ss, nil
 }
 
 //handle the root route - create the cookie - test if cookie is the same as created - then on logged in
@@ -40,25 +70,45 @@ func createTestCookie(w http.ResponseWriter, req *http.Request) {
 		fmt.Println()
 
 		//get the email from cookie
-		email := strings.SplitAfter(cookie.Value, "|")[1]
-		fmt.Println("Cookie email: ", strings.Split(cookie.Value, "|")[1])
-		fmt.Println()
+		// email := strings.SplitAfter(cookie.Value, "|")[1]
+		// fmt.Println("Cookie email: ", strings.Split(cookie.Value, "|")[1])
+		// fmt.Println()
+
 		//get the hmac from cookie
-		returnCode := strings.Split(cookie.Value, "|")[0]
-		fmt.Println("Return Code from email: ", returnCode)
-		fmt.Println()
-		code := getCode(email)
-		// cookie.Value = code + "|" + cookie.Value
-		fmt.Println("checked Code from email", code)
-		fmt.Println()
-		fmt.Println("********************************************************")
-		http.SetCookie(w, cookie)
+		// returnCode := strings.Split(cookie.Value, "|")[0]
+		// fmt.Println("Return Code from email: ", returnCode)
+		// fmt.Println()
+		// code := getCode(email)
+		// code, err := getJWT(email)
+		// if err != nil {
+		// 	http.Error(w, err.Error(), http.StatusInternalServerError) //err.Error() - type string
+		// }
+		// // cookie.Value = code + "|" + cookie.Value
+		// fmt.Println("checked Code from email", code)
+		// fmt.Println()
+		// fmt.Println("********************************************************")
+		// http.SetCookie(w, cookie)
 		// set logged in flag
 		message = "Not logged in!..."
-		isEqual = hmac.Equal([]byte(code), []byte(returnCode))
+		// isEqual = hmac.Equal([]byte(code), []byte(returnCode))
+
+		//create myCustomClaims type
+		type MyCustomClaims struct {
+			Data string `json:"myclaimsfield"`
+			jwt.StandardClaims
+		}
+
+		//get the signed token from cookie
+		// ss := cookie.Value
+
+		//check signature
+		// token, err:= jwt.ParseWithClaims(ss, &MyCustomClaims{}, jwt.Keyfunc (*){})
+
 		if isEqual {
 			message = "Logged in!..."
 		}
+
+		http.SetCookie(w, cookie)
 		//compare the new hmac to the cookie hmac
 		if !isEqual { //code != returnCode
 			cookie = &http.Cookie{} //reset cookie if not the same
@@ -117,9 +167,14 @@ func submit(w http.ResponseWriter, req *http.Request) {
 	if req.FormValue("email") != "" {
 		cookie.Value = req.FormValue("email")
 	}
-	code := getCode(email)
+	// code := getCode(email)
+	//get the code
+	code, err := getJWT(email)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError) //err.Error() - type string
+	}
 	//sign the cookie
-	cookie.Value = code + "|" + email
+	cookie.Value = code
 	http.SetCookie(w, cookie)
 	fmt.Println("********************************************************")
 	fmt.Println(" first Cookie : ", cookie.Value)
