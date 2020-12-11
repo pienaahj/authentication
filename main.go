467a45f37986fb567b27c194dfbaa0db35c1cbe7
Hendrik Pienaar <pienaahj@Hendrik-Pienaars-Mac-Pro.local>
2020-12-11 13:17:55 +0200

    ninja 02 hoe 06

--- main.go
+++ main.go
@@ -1,12 +1,9 @@
 package main
 
 import (
-	"crypto/hmac"
-	"crypto/sha256"
 	"errors"
 	"fmt"
 	"html/template"
-	"io"
 	"log"
 	"net/http"
 	"net/url"
@@ -83,9 +80,6 @@ func main() {
 	// Get a template controller value.
 	c := NewController(conf.TPL)
 
-	// http.HandleFunc("/", createTestCookie)
-	//handle the register route
-	// http.HandleFunc("/submit", submit)
 	// handle the root route
 	http.HandleFunc("/", c.register)
 	// handle the register route
@@ -99,73 +93,51 @@ func main() {
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
 
-//getCode uses input data - string and returns an HAMC - string
-func getCode(data string) string {
-	//create the hmac hash
-	h := hmac.New(sha256.New, []byte(mySigningKey))
-	//write the data passed in to the hash
-	io.WriteString(h, data)
-	//convert the hash to a string and return it
-	return fmt.Sprintf("%x", h.Sum(nil))
-}
-
-//create an HMAC token
-func getHMAC(input string) string {
-	//create the hmac hash
-	h := hmac.New(sha256.New, []byte(mySigningKey))
-	//write the data passed in to the hash
-	io.WriteString(h, input)
-	//convert the hash to a string and return it
-	return fmt.Sprintf("%x", h.Sum(nil))
-}
-
-//createToken creates a HMAC token from sessionID and retruns signature + "|" + sessionID
-func createToken(sID string) string {
-	//get the HMAC of the sessionID
-	signature := getHMAC(sID)
-	//combine the sessionID and signature
-	return signature + "|" + sID
-}
-
-//parseToken parses the HMAC token from a token and retruns the sessionID
-func parseToken(t string) (string, error) {
-	//split the token into signature and sessionID
-	sX := strings.SplitN(t, "|", 2) //only allows tor 2 indexes, safer
-	if len(sX) != 2 {
-		return "", errors.New("not hacked, nice try")
-	}
-	rSignature := sX[0]
-	sID := sX[1]
-	//verify the token
-	cSignature := createToken(sID)
-	cSignature = strings.Split(cSignature, "|")[0] //split the token out
-	if !hmac.Equal([]byte(rSignature), []byte(cSignature)) {
-		fmt.Printf("hmac's not equal: \nrSignature :%v\n cSignature : %v\n ", rSignature, cSignature)
-		return "", errors.New("not hacked, nice try")
-	}
-	return sID, nil
-}
-
-//getJWT uses input data - string and returns an JWT - string
-func getJWT(data string) (string, error) {
+// createJWT uses input data - string and returns an JWT - string
+func createJWT(sID string) (string, error) {
+	// this is tricky - embeds
 	claims := MyCustomClaims{
-		data,
+		sID,
 		jwt.StandardClaims{
-			ExpiresAt: time.Now().Add(5 * time.Minute).Unix(), //Unix() type int64
+			ExpiresAt: time.Now().Add(5 * time.Minute).Unix(), // Unix() type int64
 			Issuer:    "me",
 		},
 	}
-	//create the token
-	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims) //hmac sha256 signing method
-	//sign the token with your key
-	ss, err := token.SignedString([]byte(mySigningKey)) //hmac requires a []byte type as key
+	// create the token
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims) // hmac sha256 signing method
+	// sign the token with your key
+	ss, err := token.SignedString([]byte(mySigningKey)) // hmac requires a []byte type as key
 	if err != nil {
 		return "", fmt.Errorf("Could not sign token in SignedString: %w", err)
 	}
 	return ss, nil
 }
 
-//handle the root page GET route - /
+// parseJWT parses the JWT token from a JWT token and returns the sessionID
+func parseJWT(t string) (string, error) {
+	// check signature - this is weird!! you don't need an instance just a type of MyCustomClaims
+	// this firstly uses the token(in the callback function) and then verifies it in the same step.
+	verifiedToken, err := jwt.ParseWithClaims(t, &MyCustomClaims{}, func(unverifiedToken *jwt.Token) (interface{}, error) {
+		// according to jwt advisory you need to check if your signing method remained the same in callback.
+		// the signing method are carried inside the unverified token. The Mothod filed of the token type carries Alg() from
+		// the SigningMethod used.
+		if unverifiedToken.Method.Alg() != jwt.SigningMethodHS256.Alg() { // check that the signing method used is the one received
+			return nil, errors.New("someone tried changing the signing method")
+		}
+		return []byte(mySigningKey), nil
+	})
+
+	// Is the token valid?  It is populated when you Parse/Verify a token - only checks if the claims has not expired
+	if err == nil && verifiedToken.Valid { //there was no error and the token is valid
+		// need to assert VerifiedToken of *MyCustomeClaims type!! You know what you passed in when created.
+		// Claims type interface with valid method only
+		claims := verifiedToken.Claims.(*MyCustomClaims)
+		return claims.Data, nil
+	} // important to check the error first nill pointer value see running video
+	return "", errors.New("error while verifying token")
+}
+
+// handle the root page GET route - /
 func (c Controller) register(w http.ResponseWriter, req *http.Request) {
 	fmt.Println("Root page started")
 	//get cookie back
@@ -192,7 +164,7 @@ func (c Controller) register(w http.ResponseWriter, req *http.Request) {
 		c.tpl.ExecuteTemplate(w, "landing.gohtml", templateData)
 	} else {
 		// there is session
-		// cookieTitle := makeCookieName(email)   //get a cookie name
+		// cookieTitle := makeCookieName(email) //get a cookie name
 		cookie, err := req.Cookie(myUser.cookieName) //get the cookie back
 		fmt.Printf("There is session, Cookie: %v\n", cookie)
 		fmt.Println()
@@ -207,7 +179,9 @@ func (c Controller) register(w http.ResponseWriter, req *http.Request) {
 		oldToken := cookie.Value
 		fmt.Printf("Cookie retrieved as:\n Name: %v\n Value: %v\n", cookie.Name, cookie.Value)
 		fmt.Println()
-		sID, err := parseToken(oldToken)
+
+		// JWT parseJWT token
+		sID, err := parseJWT(oldToken)
 		if err != nil { // error parsing token - hacked!
 			msg = url.QueryEscape(err.Error())
 			http.Redirect(w, req, "/?msg= "+msg, http.StatusSeeOther)
@@ -352,7 +326,14 @@ func (c Controller) login(w http.ResponseWriter, req *http.Request) {
 	}
 	// re-create the cookie for session
 	// create the token
-	token := createToken(session[sessionID])
+	// JWT token
+	token, err := createJWT(session[sessionID])
+	if err != nil {
+		log.Printf("Error while verifying JWT token: %v\n", err)
+		msg = url.QueryEscape("Internal server error occured.")
+		http.Error(w, msg, http.StatusInternalServerError) //will display on separate page with only the error
+		return
+	}
 	// populate cookie
 	cookie = &http.Cookie{
 		Name:  session[sessionID], //email
@@ -386,146 +367,3 @@ func (c Controller) success(w http.ResponseWriter, req *http.Request) {
 
 // **************************************************************************************************
 // **************************************************************************************************
-
-//handle the root route - create the cookie - test if cookie is the same as created - then logged in
-func createTestCookie(w http.ResponseWriter, req *http.Request) {
-	//get cookie back
-	cookie, err := req.Cookie("session-id")
-	if err != nil {
-		cookie = &http.Cookie{}
-	}
-	//setting message
-	message := "Not logged in!..."
-	isEqual := true
-	// if there is a cookie process it
-	if len(cookie.Value) != 0 {
-		fmt.Println("*******************evaluating cookie****************************")
-		fmt.Println("Cookie: ", cookie.Value)
-		fmt.Println()
-
-		//get the email from cookie
-		// email := strings.SplitAfter(cookie.Value, "|")[1]
-		// fmt.Println("Cookie email: ", strings.Split(cookie.Value, "|")[1])
-		// fmt.Println()
-
-		//get the hmac from cookie
-		// returnCode := strings.Split(cookie.Value, "|")[0]
-		// fmt.Println("Return Code from email: ", returnCode)
-		// fmt.Println()
-		// code := getCode(email)
-		// code, err := getJWT(email)
-		// if err != nil {
-		// 	http.Error(w, err.Error(), http.StatusInternalServerError) //err.Error() - type string
-		// }
-		// // cookie.Value = code + "|" + cookie.Value
-		// fmt.Println("checked Code from email", code)
-		// fmt.Println()
-		// fmt.Println("********************************************************")
-		// http.SetCookie(w, cookie)
-
-		// set logged in flag
-		message = "Not logged in!..."
-		// isEqual = hmac.Equal([]byte(code), []byte(returnCode))
-
-		//get the signed token from cookie
-		ss := cookie.Value
-
-		//check signature - this is weird!! you don't need an instance just a type of MyCustomClaims
-		//this first uses the token(in the callback function) and then verifies it in the same step.
-		verifiedToken, err := jwt.ParseWithClaims(ss, &MyCustomClaims{}, func(unverifiedToken *jwt.Token) (interface{}, error) {
-			//according to jwt advisory you need to check if your signing method remained the same in callback.
-			//the signing method are carried inside the unverified token.
-			if unverifiedToken.Method.Alg() != jwt.SigningMethodHS256.Alg() {
-				return nil, fmt.Errorf("someone tried changing the signing method")
-			}
-			return []byte(mySigningKey), nil
-		})
-
-		// Is the token valid?  It is populated when you Parse/Verify a token - only checks if the claims has not expired
-		isEqual = err == nil && verifiedToken.Valid //important to check the error first nill pointer value see running video
-		//need to assert VerifiedToken of *MyCustomeClaims type!! You know what you passed in when created.
-		claims := verifiedToken.Claims.(*MyCustomClaims)
-
-		//compare the new hmac to the cookie hmac
-
-		if !isEqual { //code != returnCode
-			cookie = &http.Cookie{} //reset cookie if not the same
-		}
-		//the token is verified:
-		message = "Logged in!...as: " + claims.Data
-		fmt.Printf("Claims: %v\n email: %s\n", claims, claims.Data)
-		// cookie.Value = claims.Data
-		// http.SetCookie(w, cookie)
-		fmt.Println("*******************evaluating cookie****************************")
-		fmt.Println()
-	}
-
-	html := `<!DOCTYPE html>
-	<html>
-		<head>
-			<meta charset="utf-8">
-			<meta http-equiv="X-UA-Compatible" content="IE=edge">
-			<title>HMAC Example</title>
-			<meta name="description" content="">
-			<meta name="viewport" content="width=device-width, initial-scale=1">
-			<link rel="stylesheet" href="">
-		</head>
-		<body>
-			<p>Cookie Value: ` + cookie.Value + `</p>
-			<p>` + message + `</p>
-			<form method="POST" action="/submit">
-				<input type="email" name="email">
-				<input type="password" name="password">
-				<input type="submit">
-			</form>
-		</body>
-	</html>	
-	`
-	io.WriteString(w, html)
-}
-
-//handle the submit route
-func submit(w http.ResponseWriter, req *http.Request) {
-	if req.Method != http.MethodPost {
-		http.Redirect(w, req, "/", http.StatusSeeOther)
-		return
-	}
-	email := req.FormValue("email")
-	if email == "" {
-		http.Redirect(w, req, "/", http.StatusSeeOther)
-		return
-	}
-	password := req.FormValue("password")
-	if password == "" {
-		http.Redirect(w, req, "/", http.StatusSeeOther)
-		return
-	}
-	// set cookie
-	cookie, err := req.Cookie("session-id")
-	// cookie not set
-	if err != nil {
-		//id, _ := uuid.NewV4()
-		cookie = &http.Cookie{
-			Name: "session-id",
-		}
-	}
-	if req.FormValue("email") != "" {
-		cookie.Value = req.FormValue("email")
-	}
-	// code := getCode(email)
-	//get the code
-	code, err := getJWT(email)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError) //err.Error() - type string
-	}
-	//sign the cookie
-	cookie.Value = code
-	http.SetCookie(w, cookie)
-	fmt.Println("*******************signing cookie***********************")
-	fmt.Println(" first Cookie : ", cookie.Value)
-	fmt.Println()
-	fmt.Println("**********************signing done****************************")
-	fmt.Println()
-	http.Redirect(w, req, "/", http.StatusSeeOther)
-
-}
