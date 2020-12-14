d79fd776e43db201190682a9542eb0f03bec3131
Hendrik Pienaar <pienaahj@Hendrik-Pienaars-Mac-Pro.local>
2020-12-14 16:12:43 +0200

    getting the authenticated http client

--- main.go
+++ main.go
@@ -35,7 +35,8 @@ func main() {
 	http.HandleFunc("/", c.index)
 	// handle the startOauthGithub route
 	http.HandleFunc("/oauth/github", c.startGithubOauth)
-
+	// handle the completeGithubOauth route
+	http.HandleFunc("/oauth2/receive", c.completeGithubOauth)
 	http.Handle("/favicon.ico", http.NotFoundHandler())
 	fmt.Println("server running on port :8080")
 	log.Fatal(http.ListenAndServe(":8080", nil))
@@ -49,8 +50,31 @@ func (c Controller) index(w http.ResponseWriter, req *http.Request) {
 // handle the startGithubOauth page POST route - /
 func (c Controller) startGithubOauth(w http.ResponseWriter, req *http.Request) {
 	// You will typically use a db to hold the AuthCodeURLs for specific login attemps
-	redirectURL := githubOauthConfig.AuthCodeURL("0000")
+	redirectURL := githubOauthConfig.AuthCodeURL("0000") //code&state gets put into the redirect url on github page
 	http.Redirect(w, req, redirectURL, http.StatusSeeOther)
+}
+
+// handle the completeGithubOauth page GET route - /
+func (c Controller) completeGithubOauth(w http.ResponseWriter, req *http.Request) {
+	code := req.FormValue("code")
+	state := req.FormValue("state")
+
+	// check that the state is correct or stil valid if you have a timeout
+	if state != "0000" {
+		http.Error(w, "State is not correct!", http.StatusBadRequest)
+		return
+	}
+	// get the token back
+	token, err := githubOauthConfig.Exchange(req.Context(), code)
+	if err != nil {
+		http.Error(w, "Could not login", http.StatusInternalServerError)
+		return
+	}
+	// get the token source from token
+	ts := githubOauthConfig.TokenSource(req.Context(), token)
+	// get the http client from the token source
+	client := oauth2.NewClient(req.Context(), ts)
+
 }
 
 // 		// ********************* check valid session ****************************
