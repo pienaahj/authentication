a9f5b29d3795d1706c940d1ce38fa91fc3c56440
Hendrik Pienaar <pienaahj@Hendrik-Pienaars-Mac-Pro.local>
2020-12-14 15:44:20 +0200

    Start Oauth2 process

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/pienaahj/authentication
 
 go 1.15
 
-require github.com/dgrijalva/jwt-go v3.2.0+incompatible
+require (
+	github.com/dgrijalva/jwt-go v3.2.0+incompatible
+	golang.org/x/oauth2 v0.0.0-20201208152858-08078c50e5b5
+)
--- main.go
+++ main.go
@@ -7,11 +7,19 @@ import (
 	"net/http"
 
 	"github.com/pienaahj/authentication/conf"
+	"golang.org/x/oauth2"
+	"golang.org/x/oauth2/github"
 )
 
 // Controller struct for template controller
 type Controller struct {
 	tpl *template.Template
+}
+
+var githubOauthConfig = &oauth2.Config{
+	ClientID:     "538ea563d528f457bd46",
+	ClientSecret: "5d600c8c314322e4fadb141e8ce270bd348478c6",
+	Endpoint:     github.Endpoint,
 }
 
 // NewController provides new controller for template processing
@@ -40,7 +48,9 @@ func (c Controller) index(w http.ResponseWriter, req *http.Request) {
 
 // handle the startGithubOauth page POST route - /
 func (c Controller) startGithubOauth(w http.ResponseWriter, req *http.Request) {
-
+	// You will typically use a db to hold the AuthCodeURLs for specific login attemps
+	redirectURL := githubOauthConfig.AuthCodeURL("0000")
+	http.Redirect(w, req, redirectURL, http.StatusSeeOther)
 }
 
 // 		// ********************* check valid session ****************************
