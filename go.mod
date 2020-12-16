7323c3cb7b663b421922dd37ee2f3c87d764f5ed
Hendrik Pienaar <pienaahj@Hendrik-Pienaars-Mac-Pro.local>
2020-12-16 10:21:00 +0200

    ninja 03 hoe 02

--- go.mod
+++ go.mod
@@ -6,4 +6,5 @@ require (
 	github.com/dgrijalva/jwt-go v3.2.0+incompatible
 	github.com/gofrs/uuid v3.3.0+incompatible
 	golang.org/x/crypto v0.0.0-20200622213623-75b288015ac9
+	golang.org/x/oauth2 v0.0.0-20201208152858-08078c50e5b5
 )
--- main.go
+++ main.go
@@ -1,16 +1,23 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"html/template"
+	"io/ioutil"
 	"log"
 	"net/http"
 	"net/url"
+	"strings"
+	"time"
 
 	"github.com/gofrs/uuid"
 	"github.com/pienaahj/authentication/conf"
 
 	"golang.org/x/crypto/bcrypt"
+	"golang.org/x/oauth2"
+	"golang.org/x/oauth2/amazon"
+	"golang.org/x/oauth2/github"
 )
 
 // Controller struct for template controller
@@ -32,6 +39,34 @@ type userDB struct {
 	passW      string //unencrpted user password
 }
 
+// config for gitbub oauth2
+var githubOauthConfig = &oauth2.Config{
+	ClientID:     "538ea563d528f457bd46",
+	ClientSecret: "5d600c8c314322e4fadb141e8ce270bd348478c6",
+	Endpoint:     github.Endpoint,
+}
+
+// GithibResponse struct
+type githubResponse struct {
+	Data struct {
+		Viewer struct {
+			ID string `json: "id"`
+		} `json:"viewer"`
+	} `json:"data"`
+}
+
+// db for github connections key:github ID value:user ID
+var githubConnections map[string]string
+
+// config for gitbub oauth2
+var amazonOauthConfig = &oauth2.Config{
+	ClientID:     "amzn1.application-oa2-client.479f18b080cc4a20a8b9206834a7a596",
+	ClientSecret: "bfdca5433cd5213bb306efc835032b277e410e9b4f0440b31b6063482d860d9c",
+	Endpoint:     amazon.Endpoint,
+	RedirectURL:  "http://localhost:8080/oauth2/amazon/receive", // this is where amazon needs to return your token
+	// note most servers will only allow http requests to localhost all others needs https
+	Scopes: []string{"profile"},
+}
 var (
 	sessionID uuid.UUID
 	// create map to store sessions of UUID(sessionID) per email
@@ -40,6 +75,10 @@ var (
 	msg      string //use as message field
 	// Get a useDB instance
 	myUser = userDB{}
+	// create a state for oauth2 type uuid
+	stateUUID uuid.UUID
+	// create the oauth2 login map key: state uuid type string, value: expiration duration type time.Time
+	amazonState = map[string]time.Time{}
 )
 
 // NewController provides new controller for template processing
@@ -61,21 +100,59 @@ func main() {
 	http.HandleFunc("/success", c.success)
 	// handle the logout route
 	http.HandleFunc("/logout", c.logout)
+	// handle the amazon oauth login route POST
+	http.HandleFunc("/oauth2/amazon/login", c.oauth2AmazonLogin)
+	// handle the startOauthGithub route POST
+	http.HandleFunc("/oauth2/github/login", c.oath2GithubLogin)
+	// handle the completeGithubOauth route
+	// http.HandleFunc("/oauth2/receive", c.completeGithubOauth)
+	// handle the completeAmazonOauth route
+	http.HandleFunc("/oauth2/amazon/receive", c.completeAmazonOauth)
 
 	http.Handle("/favicon.ico", http.NotFoundHandler())
 	fmt.Println("server running on port :8080")
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
 
+// handle the oath2GithubLogin page POST route - /aouth/gitbub/login
+func (c Controller) oath2GithubLogin(w http.ResponseWriter, req *http.Request) {
+	// You will typically use a db to hold the AuthCodeURLs for specific login attemps
+	redirectURL := githubOauthConfig.AuthCodeURL("0000") //code&state gets put into the redirect url on github page
+	http.Redirect(w, req, redirectURL, http.StatusSeeOther)
+}
+
+// handle the oauth2login page POST route - /oauth2/amazon/login
+func (c Controller) oauth2AmazonLogin(w http.ResponseWriter, req *http.Request) {
+	if req.Method != http.MethodPost {
+		msg = url.QueryEscape("Your method was not POST")
+		http.Redirect(w, req, "/?msg= "+msg, http.StatusSeeOther)
+		return
+	}
+	// create the amazonState
+	stateUUID, err := uuid.NewV4()
+	if err != nil {
+		msg = url.QueryEscape(err.Error())
+		http.Redirect(w, req, "/?msg= "+msg, http.StatusSeeOther)
+		fmt.Printf("Could not create uuid for amazon state: %v", err)
+		return
+	}
+	state := stateUUID.String()
+	amazonState[state] = time.Now().Add(time.Hour) // expire the state at 1 hour
+	log.Printf("The amazon state: %v created\n with expiary time: %v\n", state, amazonState[state])
+	redirectURL := amazonOauthConfig.AuthCodeURL(state)
+	//code&state gets put into the redirect url on amazon page
+	http.Redirect(w, req, redirectURL, http.StatusSeeOther)
+}
+
 // handle the root page GET route - /
 func (c Controller) register(w http.ResponseWriter, req *http.Request) {
 	fmt.Println("Root page started")
-	//get cookie back
+	// get cookie back
 	fmt.Printf("Email: %v\n", sessions[email])
 	fmt.Println()
-	//see if there is session
-	if sessions[email] == uuid.Nil { //no session
-		//clear the cookie
+	// see if there is session
+	if sessions[email] == uuid.Nil { // no session
+		// clear the cookie
 		cookie := &http.Cookie{}
 		email = ""
 		msg = req.FormValue("msg")
@@ -94,7 +171,7 @@ func (c Controller) register(w http.ResponseWriter, req *http.Request) {
 		c.tpl.ExecuteTemplate(w, "landing.gohtml", templateData)
 	} else {
 		// there is session
-		cookie, err := req.Cookie(cookieName) //get the cookie back
+		cookie, err := req.Cookie(cookieName) // get the cookie back
 		fmt.Printf("There is session, Cookie: %v\n", cookie)
 		fmt.Println()
 		if err != nil { //no session
@@ -346,4 +423,143 @@ func (c Controller) logout(w http.ResponseWriter, req *http.Request) {
 	return
 }
 
-// **************************************************************************************************
+// **************************************** github complete *********************************************
+// handle the completeGithubOauth page GET route - /
+func (c Controller) completeGithubOauth(w http.ResponseWriter, req *http.Request) {
+	code := req.FormValue("code")
+	state := req.FormValue("state")
+
+	// check that the state is correct or stil valid if you have a timeout
+	if state != "0000" {
+		http.Error(w, "State is not correct!", http.StatusBadRequest)
+		return
+	}
+	// get the token back
+	token, err := githubOauthConfig.Exchange(req.Context(), code)
+	if err != nil {
+		http.Error(w, "Could not login", http.StatusInternalServerError)
+		return
+	}
+
+	// get the token source from token
+	ts := githubOauthConfig.TokenSource(req.Context(), token)
+
+	// get the http client from the token source you now have a client that is authenticated by the resource provider
+	client := oauth2.NewClient(req.Context(), ts)
+
+	// make a graphql request to github to look up the viewer's id
+	// construct the json query string (json keys without the values)
+	requestBody := strings.NewReader(`{"query":"query {viewer {id}}"}`)
+	// GraphQL query structure for github:
+	// { \
+	//	\"query\": \"query { viewer { login }}\" \
+	//  } \
+	resp, err := client.Post("https://api.github.com/graphql", "application/json", requestBody)
+	if err != nil {
+		http.Error(w, "Cannot get user", http.StatusInternalServerError)
+		return
+	}
+	defer resp.Body.Close()
+
+	bs, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		http.Error(w, "Cannot read github information", http.StatusInternalServerError)
+		return
+	}
+	log.Println(string(bs)) //JSON response from github: {"data":{"viewer":{"id":"MDQ6VXNlcjYzMDUzNjU0"}}}
+
+	// create a instance of githubresponse
+	var gr = githubResponse{}
+	// unmarshall github response
+	err = json.NewDecoder(resp.Body).Decode(&gr)
+	if err != nil {
+		http.Error(w, "Github invalid response!", http.StatusInternalServerError)
+		return
+	}
+	// get github id
+	githubID := gr.Data.Viewer.ID
+	// store in db
+	userID, ok := githubConnections[githubID]
+	if !ok {
+		// new user create account
+	}
+	// log into account with userID and JWT
+	fmt.Println("userID: ", userID)
+}
+
+// ************************************** amazon complete ************************************************
+// handle the completeGithubOauth page GET route - /oauth2/amazon/receive
+func (c Controller) completeAmazonOauth(w http.ResponseWriter, req *http.Request) {
+	code := req.FormValue("code")
+	stateReturned := req.FormValue("state")
+
+	// check that the state is correct or stil valid if you have a timeout
+	if rTime, ok := amazonState[stateReturned]; !ok {
+		http.Error(w, "State is not correct!", http.StatusBadRequest)
+		log.Printf("The amazon state is invalid!")
+		return
+	} else if rTime.Before(time.Now()) {
+		http.Error(w, "State expired!", http.StatusBadRequest)
+		log.Printf("The amazon state has expired! Time now: %v\n Expiry time for state: %v\n", time.Now(), rTime)
+		return
+	}
+
+	// get the token back
+	token, err := amazonOauthConfig.Exchange(req.Context(), code)
+	if err != nil {
+		http.Error(w, "Could not login", http.StatusInternalServerError)
+		log.Printf("Could not get token from amazon, for code: %v\n", code)
+		return
+	}
+	// the token received from amazon
+	log.Printf("Amazon token: %v\n", token)
+
+	// get the token source from token
+	ts := amazonOauthConfig.TokenSource(req.Context(), token)
+	log.Printf("Amazon token source: %v\n", ts)
+
+	// get the http client from the token source you now have a client that is authenticated by the resource provider
+	client := oauth2.NewClient(req.Context(), ts)
+	fmt.Printf("Amazon client: %v\n", client)
+	// amazon response
+	// http://localhost:8080/oauth2/amazon/receive?code=ANvVzJPdqkyACgzMPFBM&scope=profile&state=a3b2ca7e-324a-476f-8f0f-5294a3e84008
+
+	// make a graphql request to github to look up the viewer's id
+	// construct the json query string (json keys without the values)
+	// requestBody := strings.NewReader(`{"query":"query {viewer {id}}"}`)
+	// // GraphQL query structure for github:
+	// // { \
+	// //	\"query\": \"query { viewer { login }}\" \
+	// //  } \
+	// resp, err := client.Post("https://api.github.com/graphql", "application/json", requestBody)
+	// if err != nil {
+	// 	http.Error(w, "Cannot get user", http.StatusInternalServerError)
+	// 	return
+	// }
+	// defer resp.Body.Close()
+
+	// bs, err := ioutil.ReadAll(resp.Body)
+	// if err != nil {
+	// 	http.Error(w, "Cannot read github information", http.StatusInternalServerError)
+	// 	return
+	// }
+	// log.Println(string(bs)) //JSON response from github: {"data":{"viewer":{"id":"MDQ6VXNlcjYzMDUzNjU0"}}}
+
+	// // create a instance of githubresponse
+	// var gr = githubResponse{}
+	// // unmarshall github response
+	// err = json.NewDecoder(resp.Body).Decode(&gr)
+	// if err != nil {
+	// 	http.Error(w, "Github invalid response!", http.StatusInternalServerError)
+	// 	return
+	// }
+	// // get github id
+	// githubID := gr.Data.Viewer.ID
+	// // store in db
+	// userID, ok := githubConnections[githubID]
+	// if !ok {
+	// 	// new user create account
+	// }
+	// // log into account with userID and JWT
+	// fmt.Println("userID: ", userID)
+}
